224119395c36abf9f69d6ddaa94c1adb36bc9c30
Charumathi Badrinath <charumathi_badrinath@ryecountryday.org>
2018-07-19 22:10:07 -0400

    My first time using "define"

---
+++ definitions.h
@@ -0,0 +1,11 @@
+//
+// Created by Charumathi Badrinath on 7/19/18.
+//
+
+#ifndef LEARNING_C_DEFINITIONS_H
+#define LEARNING_C_DEFINITIONS_H
+
+#endif //LEARNING_C_DEFINITIONS_H
+
+
+#define p(...) printf(__VA_ARGS__);
